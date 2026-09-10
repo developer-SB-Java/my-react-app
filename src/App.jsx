495a2b1b0d298409ddfe55a33674d22ac7e27c1c
developer-SB-Java <developer.hub25@gmail.com>
2026-09-10 17:15:29 +0530

    chnages in config again

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Welcome from './components/Welcome'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Welcome/>
     </>
   )
 }
